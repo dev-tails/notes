d5e0dfb5dbe54b407cfdf87b7fb40d10fa675d69
Adam Berg <adam@xyzdigital.com>
2022-10-13 07:46:37 -0700

    Update some styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,46 @@
 import { addNote, getAllNotes, Note } from "./db/db";
 
+function setStyle(el: HTMLElement, style: Partial<CSSStyleDeclaration>) {
+  for (const key of Object.keys(style)) {
+    el.style[key] = style[key];
+  }
+}
+
 async function init() {
   const root = document.getElementById("root");
   if (!root) {
     return;
   }
 
+  setStyle(root, {
+    maxWidth: "800px",
+    width: "100%",
+    margin: "0 auto"
+  })
+
   const noteList = document.createElement("div");
+  setStyle(noteList, {
+    height: "calc(100vh - 50px)",
+    overflowY: "scroll",
+    overflowX: "hidden",
+    display: "flex",
+    flexDirection: "column-reverse"
+  })
   root.append(noteList);
 
   const input = document.createElement("input");
+  input.autofocus = true;
+  setStyle(input, {
+    width: "100%",
+    height: "30px",
+    padding: "10px"
+  })
   root.append(input);
 
   const NoteListItem = (note: Note) => {
     const noteDiv = document.createElement("div");
     noteDiv.innerText = note.body || "";
-    noteList.append(noteDiv);
+    noteList.prepend(noteDiv);
   };
 
   document.addEventListener("keydown", async (e) => {
@@ -24,12 +49,17 @@ async function init() {
         body: input.value,
       });
 
+      input.value = "";
+
       NoteListItem(note);
     }
   });
 
   const notes = await getAllNotes();
-  for (const note of notes) {
+  const sortedNotes = notes.sort((a,b) => {
+    return (a.createdAt as any).getTime() - (b.createdAt as any).getTime();
+  })
+  for (const note of sortedNotes) {
     NoteListItem(note);
   }
 }
